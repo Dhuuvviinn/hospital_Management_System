a756ec47f5c3b6d5eeff424d9d79a367b97b0a55
Dhuuvviinn <dhruvinpatel17000@gmail.com>
2026-04-28 12:18:47 -0400

    changes

--- Frontend/HMS/src/Redux/baseurl.js
+++ Frontend/HMS/src/Redux/baseurl.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
-export const baseURL = `${window.location.protocol}//${window.location.hostname}:8000`;
+export const baseURL = ""
+
--- Frontend/HMS/src/Redux/services/api.js
+++ Frontend/HMS/src/Redux/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const   API = axios.create({
-    baseURL: `${window.location.protocol}//${window.location.hostname}:8000`,
+    baseURL: "",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
